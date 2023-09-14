583592018e386b5dfc4c2124ed01cc885f60f490
MrSkill573 <denisdevald05@gmail.com>
2023-09-14 15:37:58 +1000

    deleted

--- test 2.py
+++
@@ -1,9 +0,0 @@
-
-def border(ip, prefix):
-    sus = ip.replace('.', '')
-    sus = sus[:prefix].ljust(32, '0')
-    sus.insert(sus.find())
-    return sus
-
-
-print(border('11111111.11111111.11111111.11111111', 28))
