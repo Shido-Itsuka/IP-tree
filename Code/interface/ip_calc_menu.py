99ad9214ea92e58ecd3c76b56dcce59ae4cae894
Shido's laptop <trusoff.maksim2017@gmail.com>
2023-11-30 16:14:55 +1000

    Clear button works now

--- Code/interface/ip_calc_menu.py
+++ Code/interface/ip_calc_menu.py
@@ -1,13 +1,31 @@
 import flet as ft
-from flet import TextField, Checkbox, Dropdown, ElevatedButton, Text, Row, Column, Container, Stack, View
+from flet import TextField, Dropdown, ElevatedButton, Text, Row, Column, Container, Stack, View
 from flet_core import View
-from flet_core.control import Control
+import sys
+
+sys.path.append('../')
+from modules.ip_calculator import IP_Calculator
+
+
+class IP_Calculator_module(IP_Calculator):
+
+    def __init__(self):
+        super().__init__()
+
 
 theme = ft.Theme(color_scheme_seed='#5a189a')
 
 
 def clear(e):
-    pass
+    IP_input.value = ""
+    IP_input.update()
+    Mask_input.value = ""
+    Mask_input.update()
+    IP_input.focus()
+
+
+def calculate(e):
+    IP_Calculator_module()
 
 
 body = Row(
@@ -48,12 +66,16 @@ body = Row(
                     ),
 
                     IP_input := TextField(
-                        label='IP-адрес',
+                        label='Введите IP-адрес',
                         hint_text='127.0.0.1',
-                        helper_text='Введите IP-адрес',
                         autofocus=True,
                         text_size=26,
                         max_length=15,
+                        input_filter=ft.InputFilter(
+                            allow=True,
+                            regex_string=r"^[0-9\.]*$",
+                            replacement_string='',
+                        )
 
                     ),
 
@@ -61,42 +83,43 @@ body = Row(
                         label='Префикс/Маска',
                         hint_text='Выберите префикс/маску',
                         options=[
-                            ft.dropdown.Option("0 - 000.000.000.000"),
-                            ft.dropdown.Option("1 - 128.000.000.000"),
-                            ft.dropdown.Option("2 - 192.000.000.000"),
-                            ft.dropdown.Option("3 - 224.000.000.000"),
-                            ft.dropdown.Option("4 - 240.000.000.000"),
-                            ft.dropdown.Option("5 - 248.000.000.000"),
-                            ft.dropdown.Option("6 - 252.000.000.000"),
-                            ft.dropdown.Option("7 - 254.000.000.000"),
-                            ft.dropdown.Option("8 - 255.000.000.000"),
-                            ft.dropdown.Option("9 - 255.128.000.000"),
-                            ft.dropdown.Option("10 - 255.192.000.000"),
-                            ft.dropdown.Option("11 - 255.224.000.000"),
-                            ft.dropdown.Option("12 - 255.240.000.000"),
-                            ft.dropdown.Option("13 - 255.248.000.000"),
-                            ft.dropdown.Option("14 - 255.252.000.000"),
-                            ft.dropdown.Option("15 - 255.254.000.000"),
-                            ft.dropdown.Option("16 - 255.255.000.000"),
-                            ft.dropdown.Option("17 - 255.255.128.000"),
-                            ft.dropdown.Option("18 - 255.255.192.000"),
-                            ft.dropdown.Option("19 - 255.255.224.000"),
-                            ft.dropdown.Option("20 - 255.255.240.000"),
-                            ft.dropdown.Option("21 - 255.255.248.000"),
-                            ft.dropdown.Option("22 - 255.255.252.000"),
-                            ft.dropdown.Option("23 - 255.255.254.000"),
-                            ft.dropdown.Option("24 - 255.255.255.000"),
-                            ft.dropdown.Option("25 - 255.255.255.128"),
-                            ft.dropdown.Option("26 - 255.255.255.192"),
-                            ft.dropdown.Option("27 - 255.255.255.224"),
-                            ft.dropdown.Option("28 - 255.255.255.240"),
-                            ft.dropdown.Option("29 - 255.255.255.248"),
-                            ft.dropdown.Option("30 - 255.255.255.252"),
-                            ft.dropdown.Option("31 - 255.255.255.254"),
-                            ft.dropdown.Option("32 - 255.255.255.255")
+                            ft.dropdown.Option(0, "0 - 000.000.000.000"),
+                            ft.dropdown.Option(1, "1 - 128.000.000.000"),
+                            ft.dropdown.Option(2, "2 - 192.000.000.000"),
+                            ft.dropdown.Option(3, "3 - 224.000.000.000"),
+                            ft.dropdown.Option(4, "4 - 240.000.000.000"),
+                            ft.dropdown.Option(5, "5 - 248.000.000.000"),
+                            ft.dropdown.Option(6, "6 - 252.000.000.000"),
+                            ft.dropdown.Option(7, "7 - 254.000.000.000"),
+                            ft.dropdown.Option(8, "8 - 255.000.000.000"),
+                            ft.dropdown.Option(9, "9 - 255.128.000.000"),
+                            ft.dropdown.Option(10, "10 - 255.192.000.000"),
+                            ft.dropdown.Option(11, "11 - 255.224.000.000"),
+                            ft.dropdown.Option(12, "12 - 255.240.000.000"),
+                            ft.dropdown.Option(13, "13 - 255.248.000.000"),
+                            ft.dropdown.Option(14, "14 - 255.252.000.000"),
+                            ft.dropdown.Option(15, "15 - 255.254.000.000"),
+                            ft.dropdown.Option(16, "16 - 255.255.000.000"),
+                            ft.dropdown.Option(17, "17 - 255.255.128.000"),
+                            ft.dropdown.Option(18, "18 - 255.255.192.000"),
+                            ft.dropdown.Option(19, "19 - 255.255.224.000"),
+                            ft.dropdown.Option(20, "20 - 255.255.240.000"),
+                            ft.dropdown.Option(21, "21 - 255.255.248.000"),
+                            ft.dropdown.Option(22, "22 - 255.255.252.000"),
+                            ft.dropdown.Option(23, "23 - 255.255.254.000"),
+                            ft.dropdown.Option(24, "24 - 255.255.255.000"),
+                            ft.dropdown.Option(25, "25 - 255.255.255.128"),
+                            ft.dropdown.Option(26, "26 - 255.255.255.192"),
+                            ft.dropdown.Option(27, "27 - 255.255.255.224"),
+                            ft.dropdown.Option(28, "28 - 255.255.255.240"),
+                            ft.dropdown.Option(29, "29 - 255.255.255.248"),
+                            ft.dropdown.Option(30, "30 - 255.255.255.252"),
+                            ft.dropdown.Option(31, "31 - 255.255.255.254"),
+                            ft.dropdown.Option(32, "32 - 255.255.255.255")
 
                         ],
-                        text_size=26,
+                        text_size=22,
+
                     ),
 
                     # Кнопка очистить + подсчитать
@@ -119,7 +142,8 @@ body = Row(
                                         shape=ft.RoundedRectangleBorder(radius=5)
                                     ),
                                     scale=1.5,
-                                    icon=ft.icons.CHECK
+                                    icon=ft.icons.CHECK,
+                                    on_click=calculate
                                 )
                             ],
                             alignment=ft.MainAxisAlignment.SPACE_BETWEEN
