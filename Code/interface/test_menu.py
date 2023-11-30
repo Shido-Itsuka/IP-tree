374df40501572a15a8bc56d62127822c0e489a74
Shido's laptop <trusoff.maksim2017@gmail.com>
2023-11-30 16:15:51 +1000

    -

--- Code/interface/test_menu.py
+++ Code/interface/test_menu.py
@@ -1,42 +1,12 @@
-import flet as ft
+import re
 
 
-def main(page: ft.Page):
-    page.title = "Routes Example"
-
-    page.window_center()
-
-    def route_change(route):
-        page.views.clear()
-        page.views.append(
-            ft.View(
-                "/",
-                [
-                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
-                ],
-            )
-        )
-        page.title = 'Home'
-        if page.route == "/store":
-            page.views.append(
-                ft.View(
-                    "/store",
-                    [
-                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
-                    ],
-                )
-            )
-            page.title = 'Store'
-        page.update()
-
-    def view_pop(view):
-        page.views.pop()
-        top_view = page.views[-1]
-        page.go(top_view.route)
-
-    page.on_route_change = route_change
-    page.on_view_pop = view_pop
-    page.go(page.route)
+def validate_ip(ip):
+    pattern = r"^[0-9\.]*$"
+    if re.match(pattern, ip):
+        return True
+    else:
+        return False
 
 
-ft.app(target=main)
+validate_ip(input("Enter IP address: "))
